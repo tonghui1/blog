c0be7bc3a0a822ea52acbd89b6b3d6efebe48dce
tonghui <472754759@qq.com>
2026-05-27 16:34:02 +0800

    去除多余config配置

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  images: {
-    unoptimized: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
 }
 
 module.exports = nextConfig
